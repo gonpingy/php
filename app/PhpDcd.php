e43cf492a670116678e4ac5d453574395a2c462a
Hiroyuki Abe <gonpingy@gmail.com>
2016-10-15 19:34:00 +0900

    phplocサンプル

--- app/PhpDcd.php
+++ app/PhpDcd.php
@@ -3,6 +3,12 @@ class PhpDcd {
   public function __construct()
   {
     $sample = new Sample('id', 'name');
-    $sample->getId();
+    $id = $sample->getId();
+
+    if ($id = 'id') {
+      echo 'if';
+    } else {
+      echo 'else';
+    }
   }
 }
