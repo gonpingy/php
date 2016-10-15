1aa3c36ced32c529d7bfecdce8672a33f2ea1464
Hiroyuki Abe <gonpingy@gmail.com>
2016-10-15 13:59:10 +0900

    phpcpdサンプル

--- app/Sample.php
+++ app/Sample.php
@@ -1,11 +1,15 @@
 <?php
 
-class Sample {
+class Sample
+{
     protected $id;
     protected $name;
 
     public function __construct($id, $name)
     {
+        if ($id) {
+            return 123;
+        }
         if (is_int($id)) {
             $this->id = $id;
             $this->name = $name;
@@ -22,5 +26,24 @@ class Sample {
     public function getName()
     {
         return $this->name;
+    }
+
+    public function constructCopy()
+    {
+        $result = pg_query($conn, "SELECT name FROM dogs");
+        if (!$result) {
+            echo "Error!.\n";
+            exit;
+        }
+
+        $arr = pg_fetch_all($result);
+
+        $cnt = count($arr);
+        for ($i = 0; $i < $cnt; $i++) {
+            $key = key($arr[$i]);
+            $data[$key][$i] = $arr[$i][$key];
+        }
+
+        pg_close($conn);
     }
 }
